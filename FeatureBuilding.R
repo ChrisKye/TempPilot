564af8d447c31294ed77eb1248963e19918ae417
chris kye <chriskye@Chris-Kyes-Macbook-2.local>
2020-12-04 21:07:59 +0800

    moved scripts to main folder and made ground truth labels

---
+++ FeatureBuilding.R
@@ -0,0 +1,25 @@
+library(data.table)
+library(dplyr)
+ratings <- fread('../Data/metadata_csv/participant_ratings.csv')
+ratings[,"id"] <- apply(ratings, 1, function (x) {paste0(x[1],"-",x[2])})
+data_y <- ratings[,c("Participant_id","Trial", "Valence", 
+                     "Arousal", "Dominance", "Liking")]
+
+labels <- apply(data_y, 1, function(x) {
+  if (x[3] > 5) val = "High"
+  else val = "Low"
+  if (x[4] > 5) arou = "High"
+  else arou = "Low"
+  if (x[5] > 5) dom = "High"
+  else dom = "Low"
+  if (x[6] > 5) lik = "High"
+  else lik = "Low"
+  return (c("Sub" = x[1], "Trial" = x[2], "Valence" = val,"Arousal" = arou,
+                   "Dominance" = dom, "Liking" = lik))
+})
+labels <- t(labels)
+labels <- as.data.table(labels)
+colnames(labels)[1:2] <- c("SubNo","TrialNo")
+
+finalLabs <- labels[rep(seq_len(nrow(ratings)), each=60),]
+
