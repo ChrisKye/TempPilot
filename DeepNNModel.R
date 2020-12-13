95b431d71c5eecbad3a1278409c4b8db333f6770
chris kye <chriskye@Chris-Kyes-Macbook-2.local>
2020-12-13 13:05:02 +0800

    final commit

--- DeepNNModel.R
+++ DeepNNModel.R
@@ -26,10 +26,14 @@ training <- data[ind==1, 1:18]
 test <- data[ind==2, 1:18]
 trainingTarget <- dominance[ind==1]
 testTarget <- dominance[ind==2]
+trainingTarget2 <- liking[ind==1]  ###
+testTarget2 <- liking[ind==1] ###
 
 ##One Hot Encoding
 trainLabs <- to_categorical(trainingTarget)
 testLabs <- to_categorical(testTarget)
+trainLabs2 <- to_categorical(trainingTarget2) ###
+testLabs2 <- to_categorical(testTarget2) ###
 
 ##Creating sequential model
 model <- keras_model_sequential()
@@ -40,13 +44,13 @@ model %>%
         layer_dense(units = 10, activation = 'relu',
                     kernel_regularizer = regularizer_l2(0.01)) %>%
         layer_dropout(rate=0.2) %>%
-        layer_dense(units = 2, activation = 'softmax')
+        layer_dense(units = 2, activation = 'sigmoid')
 summary(model)
 
 ##Compile model
 model %>% 
         compile(loss = 'binary_crossentropy',
-                optimizer = 'adam',
+                optimizer = optimizer_sgd(lr = 0.01),
                 metrics = 'accuracy')
 
 ##Model Training
@@ -57,9 +61,19 @@ history <- model %>%
             batch_size = 32,
             validation_split = 0.2)
 
+history2 <- model %>% 
+        fit(training[1:2400,],
+            trainLabs2,
+            epoch = 50,
+            batch_size = 32,
+            validation_split = 0.2) ##t
+
 ##Model Evaluation
 model %>%
         evaluate(test, testLabs)
+
+model %>%
+        evaluate(test, testLabs2) ##
 
 
 
